96eba93c791628ae360babf7b56ceddfc6092363
Pawel Foremski <pjf@foremski.pl>
2024-10-04 14:36:28 +0200

    grep finished

--- stages/grep.go
+++ stages/grep.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"net/netip"
 	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/bgpfix/bgpfix/afi"
@@ -22,10 +23,11 @@ type Grep struct {
 	opt_if_unreach bool
 
 	opt_invert      bool
-	opt_or_match    bool
-	opt_and_value   bool
+	opt_or          bool
+	opt_strict      bool
 	opt_fail_accept string
 	opt_fail_event  string
+	opt_fail_kill   bool
 	opt_parse       bool
 
 	enabled_matches int // number of different checks we must do for each message
@@ -35,7 +37,9 @@ type Grep struct {
 	opt_af          []afi.AS
 	opt_asn         []uint32
 	opt_origin      []uint32
-	opt_prefix      []netip.Prefix
+	opt_prefix      []nlri.NLRI
+	opt_prefix_len  [129]bool
+	opt_prefix_lens int // if >0, opt_prefix_len is enabled
 	opt_nexthop     []netip.Prefix
 	opt_tag         map[string]string
 }
@@ -53,26 +57,29 @@ func NewGrep(parent *core.StageBase) core.Stage {
 	f := o.Flags
 
 	f.StringSlice("if-type", nil, "run only for messages of the specified type(s)")
-	f.Bool("if-reach", false, "run only for announcements of reachable prefixes")
+	f.Bool("if-reach", false, "run only if the message announces a reachable prefix")
+	f.Bool("if-unreach", false, "run only if the message withdraws an unreachable prefix")
 
 	f.String("fail-event", "", "on match failure, emit given event and DROP the message")
 	f.String("fail-accept", "", "on match failure, emit given event and ACCEPT the message")
+	f.Bool("fail-kill", false, "on match failure, kill the session")
 
 	f.BoolP("invert", "v", false, "invert the final result: drop messages that matched successfully")
-	f.BoolP("or-match", "o", false, "require any match type success (default: require ALL match types)")
-	f.BoolP("and-value", "a", false, "require all values for match types where makes sense (default: require ANY value)")
+	f.BoolP("or", "o", false, "require any match type (default: require ALL match types)")
+	f.BoolP("strict", "s", false, "require all values if possible (default: require ANY value)")
 
 	f.StringSlice("type", nil, "require message type(s)")
 	f.Bool("parse", false, "require the messages to parse properly, do not report message parsing errors")
-
 	f.Bool("reach", false, "require announcement of reachable prefixes")
 	f.Bool("unreach", false, "require withdrawal of unreachable prefixes")
 	f.StringSlice("af", nil, "require UPDATE for given address family (format: AFI/SAFI)")
-	f.Bool("ipv4", false, "add IPV4/UNICAST to --af")
-	f.Bool("ipv6", false, "add IPV6/UNICAST to --af")
+	f.Bool("ipv4", false, "shortcut: --af IPV4/UNICAST")
+	f.Bool("ipv6", false, "shortcut: --af IPV6/UNICAST")
+	f.Bool("flowspec", false, "shortcut: --af IPV4/FLOWSPEC,IPV6/FLOWSPEC")
 	f.IntSlice("asn", nil, "require ASNs in the AS_PATH")
 	f.IntSlice("origin", nil, "require origin ASN")
-	f.StringSlice("prefix", nil, "require given IP prefixes")
+	f.StringSlice("prefix", nil, "require any of the given IP prefixes (or their subnets)")
+	f.StringSlice("prefix-len", nil, "require prefix length (use eg. 0-8 for ranges)")
 	f.StringSlice("nexthop", nil, "require NEXT_HOP inside given prefix(es)")
 	f.StringSlice("tag", nil, "require context tag values (format: key=value)")
 
@@ -84,14 +91,17 @@ func (s *Grep) Attach() error {
 
 	// logic switches?
 	s.opt_invert = k.Bool("invert")
-	s.opt_or_match = k.Bool("or-match")
-	s.opt_and_value = k.Bool("and-value")
+	s.opt_or = k.Bool("or")
+	s.opt_strict = k.Bool("strict")
 
 	// events?
 	s.opt_fail_event = k.String("fail-event")
 	s.opt_fail_accept = k.String("fail-accept")
+	s.opt_fail_kill = k.Bool("fail-kill")
 	if s.opt_fail_accept != "" && s.opt_fail_event != "" {
 		return fmt.Errorf("--fail-event and --fail-accept must not be used together")
+	} else if s.opt_fail_kill && s.opt_fail_accept != s.opt_fail_event {
+		return fmt.Errorf("--fail-kill must not be used together with other --fail-* options")
 	}
 
 	s.opt_parse = k.Bool("parse")
@@ -219,10 +229,44 @@ func (s *Grep) Attach() error {
 		if err != nil {
 			return fmt.Errorf("--prefix %s: %w", ps, err)
 		}
-		s.opt_prefix = append(s.opt_prefix, p)
+		s.opt_prefix = append(s.opt_prefix, nlri.FromPrefix(p))
 	}
 	// require UPDATEs if --prefix used
 	if len(s.opt_prefix) > 0 {
+		s.opt_type = append(s.opt_type, msg.UPDATE) // --type UPDATE
+	}
+
+	// prefix lengths
+	for _, pl := range k.Strings("prefix-len") {
+		before, after, ok := strings.Cut(pl, "-")
+
+		// parse the first (or the only)
+		v1, err := strconv.Atoi(before)
+		if err != nil {
+			return fmt.Errorf("--prefix-len %s: %w", pl, err)
+		} else if v1 < 0 || v1 > 128 {
+			return fmt.Errorf("--prefix-len %s: invalid value", pl)
+		}
+
+		// handle ranges
+		v2 := v1
+		if ok {
+			v2, err = strconv.Atoi(after)
+			if err != nil {
+				return fmt.Errorf("--prefix-len %s: %w", pl, err)
+			} else if v2 < 0 || v2 > 128 || v2 < v1 {
+				return fmt.Errorf("--prefix-len %s: invalid value", pl)
+			}
+		}
+
+		// add
+		for l := v1; l <= v2; l++ {
+			s.opt_prefix_len[l] = true
+			s.opt_prefix_lens++
+		}
+	}
+	// require UPDATEs if --prefix-len used
+	if s.opt_prefix_lens > 0 {
 		s.opt_type = append(s.opt_type, msg.UPDATE) // --type UPDATE
 	}
 
@@ -261,6 +305,9 @@ func (s *Grep) Attach() error {
 		s.enabled_matches++
 	}
 	if len(s.opt_tag) > 0 {
+		s.enabled_matches++
+	}
+	if s.opt_prefix_lens > 0 {
 		s.enabled_matches++
 	}
 	if s.enabled_matches == 0 {
@@ -357,10 +404,13 @@ func (s *Grep) check(m *msg.Msg) (accept_message bool) {
 			return
 		}
 
-		// fire an event?
-		if s.opt_fail_event != "" {
+		// handle --fail-* options
+		switch {
+		case s.opt_fail_kill:
+			s.Fatal().Stringer("msg", m).Msg("message did not match")
+		case s.opt_fail_event != "":
 			s.Event(s.opt_fail_event, m)
-		} else if s.opt_fail_accept != "" {
+		case s.opt_fail_accept != "":
 			s.Event(s.opt_fail_accept, m)
 			accept_message = true // accept the message
 		}
@@ -378,7 +428,7 @@ func (s *Grep) check(m *msg.Msg) (accept_message bool) {
 
 		// interpret the result respecting the --or-match flag
 		switch success := check(m); {
-		case s.opt_or_match: // OR logic, require any match type
+		case s.opt_or: // OR logic, require any match type
 			if success {
 				accept_message = true // accept as-is
 				return false          // stop here
@@ -457,15 +507,20 @@ func (s *Grep) check(m *msg.Msg) (accept_message bool) {
 		return
 	}
 
+	// check prefix length?
+	if !run_match(s.check_prefix_len, s.opt_prefix_lens > 0) {
+		return
+	}
+
 	// if AND, no failures so far is a success
 	// if OR, no successes so far is a failure
-	return !s.opt_or_match
+	return !s.opt_or
 }
 
 // returns: 1 stop with success, -1 stop with failure, 0 keep going
 func (s *Grep) check_if(cond bool) int {
 	switch {
-	case s.opt_and_value: // AND logic, require all values to match
+	case s.opt_strict: // AND logic, require all values to match
 		if !cond {
 			return -1 // drop as-is, stop here
 		} else {
@@ -501,7 +556,7 @@ func (s *Grep) check_tag(m *msg.Msg) bool {
 		}
 	}
 
-	return s.opt_and_value
+	return s.opt_strict
 }
 
 func (s *Grep) check_reach(m *msg.Msg) bool {
@@ -528,9 +583,9 @@ func (s *Grep) check_asn(m *msg.Msg) bool {
 		return false
 	}
 
-	// check anywhere in AS_PATH
+	// check anywhere in AS_PATH, including AS_SETs
 	for _, asn := range s.opt_asn {
-		ok := aspath.HasAsn(asn)
+		ok := aspath.HasAsn(asn, 0)
 		switch s.check_if(ok) {
 		case 1:
 			return true
@@ -539,7 +594,7 @@ func (s *Grep) check_asn(m *msg.Msg) bool {
 		}
 	}
 
-	return s.opt_and_value
+	return s.opt_strict
 }
 
 func (s *Grep) check_origin(m *msg.Msg) bool {
@@ -550,7 +605,7 @@ func (s *Grep) check_origin(m *msg.Msg) bool {
 
 	// check at AS_PATH origin
 	for _, asn := range s.opt_origin {
-		ok := aspath.HasOrigin(asn)
+		ok := aspath.HasOrigin(asn, 0)
 		switch s.check_if(ok) {
 		case 1:
 			return true
@@ -559,7 +614,7 @@ func (s *Grep) check_origin(m *msg.Msg) bool {
 		}
 	}
 
-	return s.opt_and_value
+	return s.opt_strict
 }
 
 func (s *Grep) check_nexthop(m *msg.Msg) bool {
@@ -578,7 +633,7 @@ func (s *Grep) check_nexthop(m *msg.Msg) bool {
 		}
 	}
 
-	return s.opt_and_value
+	return s.opt_strict
 }
 
 func (s *Grep) check_prefix(m *msg.Msg) bool {
@@ -587,18 +642,37 @@ func (s *Grep) check_prefix(m *msg.Msg) bool {
 	inmsg = m.Update.GetReach(inmsg)
 	inmsg = m.Update.GetUnreach(inmsg)
 
-	for _, mp := range inmsg {
-		for _, p := range s.opt_prefix {
-			// FIXME: not overlaps?, must fully contain
-			ok := p.Overlaps(mp.Prefix)
-			switch s.check_if(ok) {
-			case 1:
-				return true
-			case -1:
-				return false
-			}
+	for i := range inmsg {
+		// is the prefix covered by any --prefix value?
+		ok := inmsg[i].FindParent(s.opt_prefix) >= 0
+
+		switch s.check_if(ok) {
+		case 1:
+			return true
+		case -1:
+			return false
 		}
 	}
 
-	return s.opt_and_value
+	return s.opt_strict
+}
+
+func (s *Grep) check_prefix_len(m *msg.Msg) bool {
+	// collect all prefixes in message
+	var inmsg []nlri.NLRI
+	inmsg = m.Update.GetReach(inmsg)
+	inmsg = m.Update.GetUnreach(inmsg)
+
+	for i := range inmsg {
+		pl := inmsg[i].Bits()
+		ok := pl >= 0 && pl <= 128 && s.opt_prefix_len[pl]
+		switch s.check_if(ok) {
+		case 1:
+			return true
+		case -1:
+			return false
+		}
+	}
+
+	return s.opt_strict
 }
