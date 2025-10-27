b2e9dc32a3805275ebbd9fa558298abdf9958e0c
mwichtlh <matthias.wichtlhuber@de-cix.net>
2025-10-27 15:28:19 +0100

    Added optional local address and port to be used
    Added optional local address and port to be used for connection to remote BGP speaker.

--- stages/connect.go
+++ stages/connect.go
@@ -16,6 +16,7 @@ type Connect struct {
 	in *pipe.Input
 
 	target string
+	localaddr string
 	conn   net.Conn
 }
 
@@ -34,26 +35,40 @@ func NewConnect(parent *core.StageBase) core.Stage {
 	f.Duration("timeout", time.Minute, "connect timeout (0 means none)")
 	f.Duration("closed", time.Second, "half-closed timeout (0 means none)")
 	f.String("md5", "", "TCP MD5 password")
+	f.String("localaddr", "", "local address to bind to (IP or [IP:port])")
 	o.Args = []string{"addr"}
 
 	return s
 }
 
 func (s *Connect) Attach() error {
-	// check config
+	// check config target
 	s.target = s.K.String("addr")
 	if len(s.target) == 0 {
 		return fmt.Errorf("no target address defined")
 	}
 
 	// target needs a port number?
-	_, _, err := net.SplitHostPort(s.target)
-	if err != nil {
+	_, _, err_t := net.SplitHostPort(s.target)
+	if err_t != nil {
 		// a literal IP address?
-		if a, err := netip.ParseAddr(s.target); err == nil {
+		if a, err_t := netip.ParseAddr(s.target); err_t == nil {
 			s.target = netip.AddrPortFrom(a, 179).String()
 		} else {
 			s.target += ":179" // no idea, best-effort try
+		}
+	}
+
+	// localaddr (optional)
+	s.localaddr = s.K.String("localaddr")
+	if s.localaddr != "" {
+		// ensure it has a port; default :0 (ephemeral)
+		if _, _, err := net.SplitHostPort(s.localaddr); err != nil {
+			if a, err := netip.ParseAddr(s.localaddr); err == nil {
+				s.localaddr = netip.AddrPortFrom(a, 0).String()
+			} else {
+				s.localaddr += ":0"
+			}
 		}
 	}
 
@@ -74,6 +89,15 @@ func (s *Connect) Prepare() error {
 	// dialer
 	var dialer net.Dialer
 	dialer.Control = tcp_md5(s.K.String("md5"))
+
+	// optionally bind to a local address
+	if s.localaddr != "" {
+		laddr, err := net.ResolveTCPAddr("tcp", s.localaddr)
+		if err != nil {
+			return fmt.Errorf("invalid localaddr %q: %w", s.localaddr, err)
+		}
+		dialer.LocalAddr = laddr
+	}
 
 	// dial
 	s.Info().Msgf("dialing %s", s.target)
