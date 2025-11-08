fb928c74387eea4328006a2c205da8e55dc85b5f
Pawel Foremski <pjf@foremski.pl>
2025-11-08 01:31:37 +0100

    close/send_safe

--- stages/util.go
+++ stages/util.go
@@ -121,20 +121,36 @@ func conn_handle(s *core.StageBase, conn net.Conn, in *pipe.Input, timeout time.
 	return err
 }
 
+// close_safe closes channel ch if ch != nil.
+// It recovers from panic if the channel is already closed.
+// It returns ok=true if the channel was closed successfully.
 func close_safe[T any](ch chan T) (ok bool) {
-	if ch != nil {
-		defer func() { recover() }()
-		close(ch)
-		return true
+	if ch == nil {
+		return
 	}
-	return
+	defer func() {
+		if !ok {
+			recover()
+		}
+	}()
+	close(ch)
+	return true
 }
 
+// send_safe sends value v to channel ch, if ch != nil.
+// It recovers from panic if the channel is closed.
+// It returns ok=true if the value was sent successfully.
 func send_safe[T any](ch chan T, v T) (ok bool) {
-	if ch != nil {
-		defer func() { recover() }()
-		ch <- v
-		return true
+	if ch == nil {
+		return
 	}
-	return
+	defer func() {
+		if !ok {
+			recover()
+		}
+	}()
+	ch <- v
+	return true
+}
+
 }
